7ed06d0d9c49ef92457d242dd6156508fee54cec
Saikou Gassama <gassamasaikou@yahoo.com>
2023-08-13 21:28:04 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
+# Your code goes here
+print('hello world')
+
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
