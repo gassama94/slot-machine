ea9e40d817b3e8f136ff79894c0603a6db3eccdb
Saikou Gassama <gassamasaikou@yahoo.com>
2023-08-13 22:02:02 +0000

    created a deposit function logic for amount

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
-# Your code goes here
-print('hello world')
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number!")
+
+    return amount
+
+deposit()
+
 
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
