2262c48562d882616e4170053be4b9fe2f061aa2
Saikou Gassama <gassamasaikou@yahoo.com>
2023-08-14 13:00:33 +0000

    Succesfully implemented game logic

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import random
 print("Welcome to the Slot Machine Game!")
 
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+
 ROWS = 3
 COLS = 3
+
 
 symbol_count = {
     "A": 2,
@@ -14,6 +17,50 @@ symbol_count = {
     "C": 6,
     "D": 8,
 }
+
+
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    """
+    Calculate the winnings and winning lines in the slot machine.
+    based on the provided columns.
+
+    function checks for winning combinations on each line of the slot machine.
+    A winning combination consists of identical symbols.
+    Across all columns on a single line.
+
+    Args:
+        columns (list): List of lists representing the slot machine columns.
+        lines (int): Number of lines bet on.
+        bet (int): Bet amount per line.
+        values (dict): Dictionary containing symbol values.
+
+    Returns:
+        winnings (int): Total winnings from winning combinations.
+        winning_lines (list): List of line numbers with winning combinations.
+    """
+    winnings = 0
+    winning_lines = []
+    # Iterate through each line
+    for line in range(lines):
+        symbol = columns[0][line]   
+        # Check if all symbols on the same line are identical
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
 
 
 def get_slot_machine_spin(rows, cols, symbols):
@@ -29,19 +76,24 @@ def get_slot_machine_spin(rows, cols, symbols):
     - columns (list): List of lists representing the slot machine columns.
     """
     all_symbols = []
+    # Populate all_symbols with symbols based on their counts
     for symbol, symbol_count in symbols.items():
         all_symbols.extend([symbol] * symbol_count)
 
     columns = []
+    # Generate column data for each column
     for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
+        # Generate row data for each row in the column
         for _ in range(rows):
             value = random.choice(current_symbols)
-            current_symbols.remove(value)
+            # Ensure unique symbols
+            current_symbols.remove(value) 
             column.append(value)
 
-        columns.append(column)
+        # Add column to columns list
+        columns.append(column) 
 
     return columns
 
@@ -53,12 +105,14 @@ def print_slot_machine(columns):
     Args:
     - columns (list): List of lists representing the slot machine columns.
     """
+    # Iterate through each row
     for row in range(len(columns[0])):
+        # Print symbols for each column in the row
         for i, column in enumerate(columns):
             end_character = " | " if i != len(columns) - 1 else ""
-            print(column[row], end=end_character)
-
-        print()
+            print(column[row], end=end_character)           
+        # Print a newline after each row
+        print()  
 
 
 def deposit():
@@ -124,11 +178,7 @@ def get_bet():
     return amount
 
 
-def main():
-    """
-    The main function to run the slot machine game.
-    """
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -143,7 +193,24 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
-   
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
 
-if __name__ == "__main__":
-    main()
+
+def main():
+    """
+    The main function to run the slot machine game.
+    """
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to spin. (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
+
+main()
